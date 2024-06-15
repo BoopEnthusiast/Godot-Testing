48f7700621a4bbe8314a725d304e7de69b428bde
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-15 17:02:27 +1200

    some shit idk

--- ProceduralAnimation/Head.gd
+++ ProceduralAnimation/Head.gd
@@ -5,6 +5,7 @@ class_name ProceduralHead extends CharacterBody3D
 const SPEED = 3.0
 const ACCEL = 1.0
 const TURN_SPEED = 5.0
+const ALIGN_SPEED = 1.0
 
 # Export variables
 @export var forward_direction: Vector3 = Vector3.BACK
@@ -16,20 +17,29 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var front_floor = $FrontFloor
 @onready var floor_detector = $FloorDetector
 @onready var mesh = $Mesh
+@onready var front_floor_ray_display = $FrontFloor/RayDisplay
+@onready var floor_detector_ray_display = $FloorDetector/RayDisplay
 
 
 func _physics_process(delta):
 	if not floor_detector.is_colliding():
 		forward_direction.y -= gravity * delta
+		floor_detector_ray_display.mesh.material.albedo_color = Color.WHITE
 	else:
 		forward_direction.y += gravity * delta
+		floor_detector_ray_display.mesh.material.albedo_color = Color.BLACK
 	
 	if not front_floor.is_colliding():
-		forward_direction = forward_direction - forward_direction / 10
+		front_floor_ray_display.mesh.material.albedo_color = Color.WHITE
+	else:
+		front_floor_ray_display.mesh.material.albedo_color = Color.BLACK
+	
+	#forward_direction = forward_direction.move_toward(Vector3.DOWN, ALIGN_SPEED)
 	
 	forward_direction = forward_direction.normalized()
 	rotation.x = forward_direction.x
-	mesh.rotation = forward_direction
+	#mesh.rotation = forward_direction
+	
 	
 	velocity = velocity.move_toward(forward_direction * SPEED, ACCEL)
 	
